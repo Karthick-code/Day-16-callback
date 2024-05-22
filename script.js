5e64583de5dde4e062f9bb300ad30cba6b393725
Karthick R <106647257+Karthick-code@users.noreply.github.com>
2024-05-22 19:32:34 +0200

    updating changes
    changes for code: code requirement doesn't include looping, i have solved the problem with looping statement earlier, here i am updating the code without looping

--- script.js
+++ script.js
@@ -9,38 +9,17 @@ document.body.append(parent_div);
 
 function print_data(callback){
     var x=0;
-    setTimeout(()=>{
-        parent_div.innerHTML="10";
-    },x+=1000);
-    setTimeout(()=>{
-        parent_div.innerHTML="9";
-    },x+=1000);
-    setTimeout(()=>{
-        parent_div.innerHTML="8";
-    },x+=1000);
-    setTimeout(()=>{
-        parent_div.innerHTML="7";
-    },x+=1000);
-    setTimeout(()=>{
-        parent_div.innerHTML="6";
-    },x+=1000);
-    setTimeout(()=>{
-        parent_div.innerHTML="5";
-    },x+=1000);
-    setTimeout(()=>{
-        parent_div.innerHTML="4";
-    },x+=1000);
-    setTimeout(()=>{
-        parent_div.innerHTML="3";
-    },x+=1000);
-    setTimeout(()=>{
-        parent_div.innerHTML="2";
-    },x+=1000);
-    setTimeout(()=>{
-        parent_div.innerHTML="1";
-    },x+=1000);
-    callback("Happy Independence Day",x)
-    
+    x=callback("10",x);
+    x=callback("9",x);
+    x=callback("8",x);
+    x=callback("7",x);
+    x=callback("6",x);
+    x=callback("5",x);
+    x=callback("4",x);
+    x=callback("3",x);
+    x=callback("2",x);
+    x=callback("1",x);
+    x=callback("Happy Independence Day",x) ;   
 
 }
 
@@ -48,4 +27,5 @@ function display(data,x){
     setTimeout(()=>{
         parent_div.innerHTML=data;
     },x+=1000);
+    return x;
 }
