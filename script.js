a9ea364d87391d1feb2934335d1db68be550a2d9
Karthick R <106647257+Karthick-code@users.noreply.github.com>
2024-05-22 19:10:10 +0200

    updating changes
    changes for code: code requirement doesn't include looping, i have solved the problem with looping statement earlier, here i am updating the code without looping

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-var parent_div = document.createElement("div");
-parent_div.className = "main";
-parent_div.setAttribute("style", "font-size: 100px; margin: 250px;text-align: center;")
+var parent_div=document.createElement("div");
+parent_div.className="main";
+parent_div.setAttribute("style","font-size: 100px; margin: 250px;text-align: center;")
 
-print_data(print_day)//print_data prints numbers from 10-1 & print_day prints happy independence day
+print_data(display);
 
 document.body.append(parent_div);
 
-function print_day(data) {
-    setTimeout(() => {
-        parent_div.innerHTML = data;
-    }, 10000);
+
+function print_data(callback){
+    var x=0;
+    setTimeout(()=>{
+        parent_div.innerHTML="10";
+    },x+=1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="9";
+    },x+=1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="8";
+    },x+=1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="7";
+    },x+=1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="6";
+    },x+=1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="5";
+    },x+=1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="4";
+    },x+=1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="3";
+    },x+=1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="2";
+    },x+=1000);
+    setTimeout(()=>{
+        parent_div.innerHTML="1";
+    },x+=1000);
+    callback("Happy Independence Day",x)
+    
 
 }
 
-function print_data(callback) {
-    var x = 0;
-    for (let i = 10; i > 0; i--) {
-        setTimeout(() => {
-            parent_div.innerHTML = i;
-        }, x);
-        x += 1000;
-    }
-    callback("Happy Independence Day");
+function display(data,x){
+    setTimeout(()=>{
+        parent_div.innerHTML=data;
+    },x+=1000);
 }
